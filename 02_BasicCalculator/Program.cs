53bf301ccae4e7e681816e93354db88fbe8d8a94
ArmandoG <armandopinedag@outlook.com>
2024-10-09 19:07:25 -0600

    exception handling with try catch block

--- 02_BasicCalculator/Program.cs
+++ 02_BasicCalculator/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+
 int operand1, operand2;
 int result = 0; //initialized (final log purpose)
 char operatorSymbol;
@@ -6,47 +7,66 @@ char operatorSymbol;
 Console.WriteLine("Basic Calculator");
 Console.WriteLine("-----------------");
 
-Console.WriteLine("Please, enter a whole number (first operand):");
-operand1 = int.Parse(Console.ReadLine()); //parse to Integer 
-
-Console.WriteLine("Please, enter a whole number (second operand):");
-operand2 = int.Parse(Console.ReadLine()); //parse to Integer 
-
-Console.WriteLine("Please, enter a valid operator symbol:");
-Console.WriteLine("'+', '-', '*', '/'");
-operatorSymbol = char.Parse(Console.ReadLine()); //convert to str to char
-
-//operator case:
-switch (operatorSymbol)
+try
 {
-    case '+':
-        checked
-        {
-            result = operand1 + operand2;
-        }
-        break;
-    case '-':
-        checked
-        {
-            result = operand1 - operand2;
-        }
-        break;
-    case '*':
-        checked // force the CLR to perform overflow checks by this 'Int' vals
-        {
-            result = operand1 * operand2;
-        }
-        break;
-    case '/':
-        checked
-        {
-        result = operand1 / operand2;
-        }
-        break;
-    default:
-        break;
+        
+    Console.WriteLine("Please, enter a whole number (first operand):");
+    operand1 = int.Parse(Console.ReadLine()); //parse to Integer 
+
+    Console.WriteLine("Please, enter a whole number (second operand):");
+    operand2 = int.Parse(Console.ReadLine()); //parse to Integer 
+
+    Console.WriteLine("Please, enter a valid operator symbol:");
+    Console.WriteLine("'+', '-', '*', '/'");
+    operatorSymbol = char.Parse(Console.ReadLine()); //convert to str to char
+
+    //operator case:
+    switch (operatorSymbol)
+    {
+        case '+':
+            checked
+            {
+                result = operand1 + operand2;
+            }
+            break;
+        case '-':
+            checked
+            {
+                result = operand1 - operand2;
+            }
+            break;
+        case '*':
+            checked // force the CLR to perform overflow checks by this 'Int' vals
+            {
+                result = operand1 * operand2;
+            }
+            break;
+        case '/':
+            checked
+            {
+            result = operand1 / operand2;
+            }
+            break;
+        default:
+            break;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"{operand1} {operatorSymbol} {operand2} = {result} ");
+}
+catch (Exception ex)
+{
+    //Console.Clear(); //Clear Console to display Error Msg
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Red;
+    
+    Console.WriteLine(ex.Message);
+    
+    Console.ResetColor();
+}
+finally
+{
+    Console.WriteLine("Press any key to exit program...");
 }
 
-Console.WriteLine();
-Console.WriteLine($"{operand1} {operatorSymbol} {operand2} = {result} ");
 Console.ReadKey(); //Esperar por user input
