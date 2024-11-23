bd3d9d5bd02d6dcc85230b5eab6cc4aef5f7bfa1
ArmandoG <armandopinedag@outlook.com>
2024-11-22 23:38:04 -0600

    add console proj about delegates

---
+++ 03_Delegates/ExampleClass.cs
@@ -0,0 +1,6 @@
+namespace _03_Delegates;
+
+public class ExampleClass
+{
+    
+}
---
+++ 03_Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03_Delegates;
+
+public class Program
+{
+    delegate void LogDelegate(string text); //delegate definition
+    
+    public static void Main(string[] args)
+    {
+        LogDelegate logDelegate = new LogDelegate(LogTextToScreen);
+        Console.WriteLine("Hello, what's your name?");
+        
+        var name = Console.ReadLine();
+        
+        logDelegate("Hello " + name);
+
+        Console.ReadKey();
+    }
+    
+    static void LogTextToScreen(string text)
+    {
+        Console.WriteLine($"{DateTime.Now} {text}");
+    }
+}
