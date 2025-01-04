a043bc2342460c300bd0f03ec2bd69677fcdbd32
ArmandoG <armandopinedag@outlook.com>
2025-01-04 09:23:23 -0600

    create basic project for fundamentals

---
+++ 01_Basics/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// consts
+static int ReturnLargestInteger()
+{
+    return int.MaxValue;
+}
+Console.WriteLine(ReturnLargestInteger());
+static int ReturnSmallestInteger()
+{
+    return int.MinValue;
+}
+Console.WriteLine(ReturnSmallestInteger());
