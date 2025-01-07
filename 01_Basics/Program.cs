ebd7e0ac12dbef01aac1bf241f0f958713216b20
ArmandoG <armandopinedag@outlook.com>
2025-01-06 22:14:58 -0600

    add constants class to project

--- 01_Basics/Program.cs
+++ 01_Basics/Program.cs
@@ -1,15 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using _01_Basics;
+
 Console.WriteLine("Hello, World!");
 
-// consts
-static int ReturnLargestInteger()
-{
-    return int.MaxValue;
-}
-Console.WriteLine(ReturnLargestInteger());
-static int ReturnSmallestInteger()
-{
-    return int.MinValue;
-}
-Console.WriteLine(ReturnSmallestInteger());
+//Consts
+Console.WriteLine(Constants.ReturnLargestInteger());
+Console.WriteLine(Constants.ReturnSmallestInteger());
+Console.WriteLine(Constants.ReturnLargestUnsignedInteger());
+Console.WriteLine(Constants.ReturnSmallestUnsignedInteger());
+Console.WriteLine(Constants.ReturnLargestLongInteger());
