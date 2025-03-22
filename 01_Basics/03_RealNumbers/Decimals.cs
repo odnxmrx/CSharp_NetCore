67afcfe165b4b0b1e5da45d8056d4f99b9d20ca8
odnxmrx <armandopinedag@outlook.com>
2025-03-21 23:02:21 -0600

    real numbers practice

---
+++ 01_Basics/03_RealNumbers/Decimals.cs
@@ -0,0 +1,36 @@
+namespace _01_Basics._03_RealNumbers;
+
+public class Decimals
+{
+    public static decimal ReturnDecimal51()
+    {
+        return 0.0m;
+    }
+
+    public static decimal ReturnDecimal52()
+    {
+        return 0.1e-6m;
+    }
+
+    public static decimal ReturnDecimal53()
+    {
+        // TODO #5-3. Return "-10,000.0000000001" literal.
+        return -10000.0000000001m;
+    }
+
+    public static decimal ReturnDecimal54()
+    {
+        return 1048294829438549029840452834.109492298482m;
+    }
+
+    public static decimal ReturnDecimal55()
+    {
+        return -30492996837502378502387459851m;
+    }
+
+    public static decimal ReturnDecimal56()
+    {
+        // TODO #5-6. Return "0.6 + 0.1" expression.
+        return 0.6m + 0.1m;
+    }
+}
---
+++ 01_Basics/03_RealNumbers/Doubles.cs
@@ -0,0 +1,38 @@
+namespace _01_Basics._03_RealNumbers;
+
+public class Doubles
+{
+    public static double ReturnDouble41()
+    {
+        return 0.0d;
+    }
+
+    public static double ReturnDouble42()
+    {
+        return 0.0001d;
+    }
+
+    public static double ReturnDouble43()
+    {
+        // TODO #4-3. Return "-10,000.0000000001" literal.
+        return -10_000.0000000001d;
+    }
+
+    public static double ReturnDouble44()
+    {
+        // TODO #4-4. Return "1,048,294,829,438,549,029,840,452,834.109492298482" literal.
+        return 1048294829438549029840452834.109492298482D;
+    }
+
+    public static double ReturnDouble45()
+    {
+        // TODO #4-5. Return "-30,492,996,837,502,378,502,387,459,850,243.942692284652825" literal.
+        return -3.049299683750238E+31D;
+    }
+
+    public static double ReturnDouble46()
+    {
+        // TODO #4-6. Return "0.6 + 0.1" expression.
+        return 0.6d + 0.1d;
+    }
+}
