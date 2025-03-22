eeb7fb984efc0f20d9ddb519578cb96399d58d4d
odnxmrx <armandopinedag@outlook.com>
2025-03-21 23:03:52 -0600

    add float examples

---
+++ 01_Basics/03_RealNumbers/Floats.cs
@@ -0,0 +1,55 @@
+namespace _01_Basics._03_RealNumbers;
+
+public class Floats
+{
+    public static float ReturnFloat31()
+    {
+        return 0;
+    }
+
+    public static float ReturnFloat32()
+    {
+        return 1.01F;
+    }
+
+    public static float ReturnFloat33()
+    {
+        return -0.01f;
+    }
+
+    public static float ReturnFloat34()
+    {
+        return 1_048_294_829_438_549_029_840_452_834.109_492_298_482f;
+    }
+
+    public static float ReturnFloat35()
+    {
+        return -30_492_996_837_502_378_502_387_459_850_243.942_692_284_652_825F;
+    }
+
+    public static float ReturnFloat36()
+    {
+        return -1.23e-40f;
+    }
+
+    public static float ReturnFloat37()
+    {
+        return -1.23e-40f;
+    }
+
+    public static float ReturnFloat38()
+    {
+        return 1_048_294_829_438_549_029_840_452_834.109_492_298_482F;
+    }
+
+    public static float ReturnFloat39()
+    {
+        return -30_492_996_837_502_378_502_387_459_850_243.942_692_284_652_825F;
+    }
+
+    public static float ReturnFloat310()
+    {
+        // TODO #3-10. Return "0.6 + 0.1" expression.
+        return 0.7f;
+    }
+}
